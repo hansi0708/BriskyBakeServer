992404af089e04452477744030c2991410b245ff
VINEETA <vineetasharma882@gmail.com>
2021-05-07 13:37:22 +0530

    commit 10th

--- app/src/main/java/com/hv/briskybakeserver/ViewHolder/MenuViewHolder.java
+++ app/src/main/java/com/hv/briskybakeserver/ViewHolder/MenuViewHolder.java
@@ -1,5 +1,6 @@
 package com.hv.briskybakeserver.ViewHolder;
 
+import android.view.ContextMenu;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -7,10 +8,13 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.hv.briskybakeserver.Common.Common;
 import com.hv.briskybakeserver.Interface.ItemClickListener;
 import com.hv.briskybakeserver.R;
 
-public class MenuViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+public class MenuViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener,
+        View.OnCreateContextMenuListener
+{
 
     public TextView textMenuName;
     public ImageView imageView;
@@ -23,6 +27,7 @@ public class MenuViewHolder extends RecyclerView.ViewHolder implements View.OnCl
         textMenuName=itemView.findViewById(R.id.menu_name);
         imageView=itemView.findViewById(R.id.menu_image);
 
+        itemView.setOnCreateContextMenuListener(this);
         itemView.setOnClickListener(this);
     }
 
@@ -33,5 +38,13 @@ public class MenuViewHolder extends RecyclerView.ViewHolder implements View.OnCl
     @Override
     public void onClick(View v) {
         itemClickListener.onClick(v,getAdapterPosition(),false);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        menu.setHeaderTitle("Select the action");
+
+        menu.add(0,0,getAdapterPosition(), Common.UPDATE);
+        menu.add(0,1,getAdapterPosition(), Common.DELETE);
     }
 }
